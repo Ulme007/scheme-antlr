5dd6d46fe9722d96402985b620cc14d3a73a0134
Ulrich Tauchert <ulrich.tauchert@epost-dev.de>
2017-12-30 22:58:41 +0100

    Add types

---
+++ src/main/java/com/ulme/antlr/calc/types/ListType.java
@@ -0,0 +1,4 @@
+package com.ulme.antlr.calc.types;
+
+public class ListType extends Type {
+}
---
+++ src/main/java/com/ulme/antlr/calc/types/Type.java
@@ -0,0 +1,7 @@
+package com.ulme.antlr.calc.types;
+
+public abstract class Type {
+
+    public Type() {
+    }
+}
